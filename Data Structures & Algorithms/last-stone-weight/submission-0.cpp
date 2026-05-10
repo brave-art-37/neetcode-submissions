5ae4f1ace1311e39ee4dd57fcb5fe27c92417a78
brave-art-37 <theaaravbhatt@gmail.com>
2026-05-10 12:44:43 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int num:stones) pq.push(num);
+
+        while(pq.size()>1){
+            int x=pq.top();pq.pop();
+            int y=pq.top();pq.pop();
+            if(x!=y) pq.push(abs(x-y));
+        }
+
+        if(pq.empty()) return 0;
+        else if(pq.size()==1) return pq.top();
+    }
+};
