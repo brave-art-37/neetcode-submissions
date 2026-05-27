f5bcec347cab505674ac90f76b7a25cac96b5b54
brave-art-37 <theaaravbhatt@gmail.com>
2026-05-27 20:20:36 +0530

    Add: min-cost-to-connect-points - submission-2

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+/*
+    int dist(vector<int>& a, vector<int>& b){
+        return abs(a[0]-b[0])+abs(a[1]-b[1]);
+    }
+*/
+    int minCostConnectPoints(vector<vector<int>>& pts) {
+        int score=0;
+        unordered_set<int> used, notUsed;
+        used.insert(0);
+        for(int i=1;i<pts.size();i++) notUsed.insert(i);
+        while(!notUsed.empty()){
+            int mini=INT_MAX, minIdx=-1;
+            for(int i:used){
+                for(int j:notUsed){
+                    int val = abs(pts[i][0]-pts[j][0])+abs(pts[i][1]-pts[j][1]);
+                    if(val>=mini) continue;
+                    mini = val;
+                    minIdx = j;
+                }
+            }
+            notUsed.erase(minIdx);
+            used.insert(minIdx);
+            score += mini;
+        }
+        return score;
+    }
+};
