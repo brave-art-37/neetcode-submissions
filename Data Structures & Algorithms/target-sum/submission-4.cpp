1cbbf0330b719900056f7f28a2d48904cf6e18ae
brave-art-37 <theaaravbhatt@gmail.com>
2026-05-30 11:14:26 +0530

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<unordered_map<int,int>> dp(n);
+        dp[0][-nums[0]] += 1;
+        dp[0][nums[0]] += 1;
+        for(int i=1;i<n;i++){
+            for(auto [amt,ways]:dp[i-1]){
+                dp[i][amt-nums[i]] += ways;
+                dp[i][amt+nums[i]] += ways;
+            }
+        }
+        if(!dp[n-1].count(target)) return 0;
+        return dp[n-1][target];
+    }
+};
